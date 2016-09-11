4a51c4e80b3dbeb8fab6b7c6d9269833fe244db7
Nadia Widy <nadiawidyoktaviani@gmail.com>
2016-09-11 08:05:18 +0700

    spinner & checkbox checking

--- app/src/main/java/id/sch/smktelkom_mlg/learn/macregistrationform/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/macregistrationform/MainActivity.java
@@ -3,16 +3,29 @@ package id.sch.smktelkom_mlg.learn.macregistrationform;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.Spinner;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
     EditText isinama;
     RadioButton rbL, rbP;
     Button bOK;
     TextView tvHasil, tvJk;
+    CheckBox cbD, cbA, cbT;
+    Spinner spJurusan, spKelas;
+    String arKelas[][] = {{"X-RPL1", "X-RPL2", "X-RPL3", "X-RPL4", "X-RPL5", "X-RPL6"},
+            {"X-TKJ1", "X-TKJ2", "X-TKJ3", "X-TKJ4", "X-TKJ5", "X-TKJ6"}};
+    ArrayList<String> listKelas = new ArrayList<>();
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,11 +38,35 @@ public class MainActivity extends AppCompatActivity {
         bOK = (Button) findViewById(R.id.buttonSubmit);
         tvHasil = (TextView) findViewById(R.id.textViewHasil);
         tvJk = (TextView) findViewById(R.id.textView2);
+        spJurusan = (Spinner) findViewById(R.id.spinnerJurusan);
+        spKelas = (Spinner) findViewById(R.id.spinnerKelas);
+        cbD = (CheckBox) findViewById(R.id.checkBoxDance);
+        cbA = (CheckBox) findViewById(R.id.checkBoxAkustik);
+        cbT = (CheckBox) findViewById(R.id.checkBoxTeather);
 
         findViewById(R.id.buttonSubmit).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 doClick();
+            }
+        });
+
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, listKelas);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spKelas.setAdapter(adapter);
+
+        spJurusan.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int pos, long id) {
+                listKelas.clear();
+                listKelas.addAll(Arrays.asList(arKelas[pos]));
+                adapter.notifyDataSetChanged();
+                spKelas.setSelection(0);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
             }
         });
     }
@@ -48,9 +85,21 @@ public class MainActivity extends AppCompatActivity {
                 jk = rbP.getText().toString();
             }
 
-            //hasil
-            tvHasil.setText("Name        : " + nama + "\n" + "Gender      : " + jk + "\n");
+            //spinner
+            String kelas = spKelas.getSelectedItem().toString();
 
+            //checkbox
+            String bidang = "Object Of Interest in MAC :\n";
+            int startlen = bidang.length();
+            if (cbD.isChecked()) bidang += cbD.getText() + "\n";
+            if (cbA.isChecked()) bidang += cbA.getText() + "\n";
+            if (cbT.isChecked()) bidang += cbT.getText() + "\n";
+
+            if (bidang.length() == startlen) bidang += "(No object was choosen)";
+
+            //hasil
+            tvHasil.setText("Name        : " + nama + "\n" + "Gender      : " + jk + "\n"
+                    + "Class         : " + kelas + "\n" + bidang);
         }
     }
 
